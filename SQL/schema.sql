e78f4ebe1c34637d0fa8453c32d951ea85c2d079
Hack Reactor Students <alexmckenley@gmail.com>
2014-01-31 12:05:02 -0800

    added database schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,13 +1,88 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
-
-CREATE TABLE messages (
- /* Describe your table here.*/
-);
 
 /* You can also create more tables, if you need them... */
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR(255) NULL DEFAULT NULL,
+  `id_users` TINYINT NULL DEFAULT NULL,
+  `id_rooms` TINYINT NULL DEFAULT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(25) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` TINYINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR(50) NOT NULL DEFAULT 'lobby',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_rooms) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`text`,`id_users`,`id_rooms`,`createdAt`) VALUES
+-- ('','','','','');
+-- INSERT INTO `users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`roomname`) VALUES
+-- ('','');
+
